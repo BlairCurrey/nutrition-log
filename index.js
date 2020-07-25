fc889ec1240c5be6011fd2fbed9aae2bfd6e2bf1
Blair Currey <curreyb88@gmail.com>
2020-07-25 15:19:22 -0400

    added db config, disconnect handling, environment detection

--- index.js
+++ index.js
@@ -5,19 +5,67 @@ const mysql = require ("mysql");
 const app = express();
 const port = process.env.PORT || 8089;
 
-const db = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'root',
-    database: 'nutrition_log'
-});
+//database connection configs for dev and production
+var db_config = {
+    production: {
+        host     : 'us-cdbr-east-02.cleardb.com',
+        user     : 'bfebd2a53eef29',
+        password : 'c113c518',
+        database : 'heroku_730d5b06561ccc0',
+        name     : 'production'
+    },
+    development: {
+        host     : 'localhost',
+        user     : 'root',
+        password : 'root',
+        database : 'nutrition_log',
+        name     : 'development'
+    }
+};
 
-//Connect to MySQL database
-db.connect((err)=> {
-    if(err) throw err;
-    console.log('Database connection established')
-});
-global.db = db;
+//detect if production or development
+var config;
+if (process.env.NODE_ENV == 'production'){
+    config = db_config.production;
+} else {
+    config = db_config.development;
+}
+
+//Solution for handling disconnects found here
+//https://stackoverflow.com/questions/20210522/nodejs-mysql-error-connection-lost-the-server-closed-the-connection
+var db;
+function handleDisconnect() {
+    db = mysql.createConnection(config); // Recreate the connection, since
+                                         // the old one cannot be reused.
+
+    db.connect(function(err) {                        // The server is either down
+        if(err) {                                     // or restarting (takes a while sometimes).
+            console.log(`error when connecting to db ${config.name}:`, err);
+            setTimeout(handleDisconnect, 2000);       // We introduce a delay before attempting to reconnect,
+        }
+        console.log(`Database connection established (${config.name})`)// to avoid a hot loop, and to allow our node script to
+    });                                               // process asynchronous requests in the meantime.
+                                                      // If you're also serving http, display a 503 error.
+    db.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                       // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                // server variable configures this)
+        }
+    });
+    global.db = db; //Reset global variable upon reconnect (routes will use old connection)
+}
+
+handleDisconnect();
+
+// Ping MySQL to keep connection open
+// Alternative to handleDisconnect()
+
+// setInterval(function () {
+//     console.log("pinging mysql server");
+//     db.query('SELECT 1');
+// }, 10000);
 
 app.use(body_parser.urlencoded({ extended: true }));
 app.use(method_override('_method'));
